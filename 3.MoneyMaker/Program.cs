8e6ee245aeb99a4063a8b01b5f946dbbaffef5d1
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-07-30 17:20:44 +0100

    basic logic implemented

--- 3.MoneyMaker/Program.cs
+++ 3.MoneyMaker/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MoneyMaker
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {   
+      Console.WriteLine("Welcome to Money Maker!");  
+      Console.WriteLine("Enter an amount you'd like to convert to coins: ");
+      
+      string totalAsString = Console.ReadLine();
+      Console.WriteLine(totalAsString);
+      
+      double total = Convert.ToDouble(totalAsString);
+      Console.WriteLine($"{total} cents is equal too..");
+      
+      int goldValue = 10;
+      int silverValue = 5;
+      
+      double goldCoins = Math.Floor(total  / goldValue);
+      double remainder = total % goldValue;
+      double silverCoins = Math.Floor(remainder / silverValue);
+      remainder = remainder % silverValue;
+      
+      Console.WriteLine($"Gold coins: {goldCoins}");
+      Console.WriteLine($"Silver coins: {silverCoins}");
+      Console.WriteLine($"Bronze coins: {remainder}");      
+    }
+  }
+}
